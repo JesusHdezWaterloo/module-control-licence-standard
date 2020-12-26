fc19d9e750d2eba15df7b0a520b291b9f31ba30c
Unknown <unknown@example.com>
2020-12-26 12:12:33 -0500

    +ModuleLicenceConstants

---
+++ src/main/java/com/jhw/module/util/licence/core/ModuleLicenceConstants.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jhw.module.util.licence.core;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
+ */
+public class ModuleLicenceConstants {
+
+    public static final String LICENCE_GENERAL_PATH = "/licence";
+
+    //-----------------------LICENCE-----------------------\\
+    public static final String LICENCE_LICENCE_GENERAL_PATH = LICENCE_GENERAL_PATH + "/licence";
+
+    //public static final String GASTO_REPORTE_POR_TIPO_PATH = "/reporte_por_tipo";
+    //public static final RequestMethod GASTO_REPORTE_POR_TIPO_METHOD = RequestMethod.GET;
+
+}
