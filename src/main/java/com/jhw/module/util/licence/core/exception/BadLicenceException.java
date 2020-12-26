f39ed5544415f73a534f780608727131d33608b5
Unknown <unknown@example.com>
2020-12-26 12:12:23 -0500

    +BadLicenceException: Movida desde el server

---
+++ src/main/java/com/jhw/module/util/licence/core/exception/BadLicenceException.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jhw.module.util.licence.core.exception;
+
+/**
+ *
+ * @author Jesus Hernandez Barrios (jhernandezb96@gmail.com)
+ */
+public class BadLicenceException extends Exception {
+
+    public BadLicenceException() {
+    }
+
+    public BadLicenceException(String string) {
+        super(string);
+    }
+
+}
